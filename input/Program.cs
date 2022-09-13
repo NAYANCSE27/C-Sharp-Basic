781e5775f7de40e9afd6ea93c2455f25ff68c25b
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-09-13 15:46:42 +0600

    user input

---
+++ input/Program.cs
@@ -0,0 +1,19 @@
+﻿string firstName;
+string lastName;
+
+Console.WriteLine("What is your name?");
+
+Console.Write("Enter your first name: ");
+firstName = Console.ReadLine();
+
+Console.Write("Enter your last name: ");
+lastName = Console.ReadLine();
+
+int age;
+string sage;
+Console.Write("Tell me how old you are: ");
+sage = Console.ReadLine();
+age=Convert.ToInt32(sage);
+
+Console.WriteLine("Hello " + firstName + ' ' + lastName + "!.....");
+Console.WriteLine("You are only " + age + " years old");
