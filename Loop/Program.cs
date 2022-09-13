e63afb87c2df91850e1b8ac0fbcf25eb63e9caac
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-09-14 01:12:12 +0600

    Different types of loop in C#

---
+++ Loop/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+/*
+for(int i=0; i<n; i++)
+{
+    if(i%3==0 && i%5==0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i%3==0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if(i%5==0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+*/
+
+int i = 0;
+while (i < n)
+{
+    Console.WriteLine(i);
+    i++;
+    if (i == 7) break;
+}
