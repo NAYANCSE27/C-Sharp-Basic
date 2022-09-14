4eea086285b54be0a60d9b5eb85a0d3fca67272a
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-09-14 12:00:05 +0600

    methods in C#

---
+++ simpleMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿string firstName;
+string lastName;
+string email;
+
+Console.Write("Enter your first name: ");
+firstName = Console.ReadLine();
+
+Console.Write("Enter your last name: ");
+lastName = Console.ReadLine();
+
+Console.Write("Enter your email: ");
+email = Console.ReadLine();
+
+/*
+display(firstName, lastName, email);
+
+static void display(string fName, string lName, string email)
+{
+    Console.Write("Your name and email: ");
+    Console.WriteLine(String.Format("{0} {1} {2}",fName, lName, email));
+}
+*/
+
+
+
+display(firstName, lastName, email);
+display(firstName + " " + lastName + " " + email);
+
+static void display(string fName, string lName, string email)
+{
+    Console.Write("Your name and email: ");
+    Console.WriteLine(String.Format("{0} {1} {2}", fName, lName, email));
+}
+
+
+static void display(string message)
+{
+    Console.WriteLine("Your name and email: ");
+    Console.Write(message);
+}
