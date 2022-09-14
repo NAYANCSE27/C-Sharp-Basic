73a51c87991c43402c3926d643b65568cbd376a5
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-09-14 21:36:28 +0600

    playing with date and time in c#

---
+++ DateTime/Program.cs
@@ -0,0 +1,15 @@
+﻿DateTime time=DateTime.Now;
+
+Console.WriteLine(time.ToString());
+Console.WriteLine(time.ToShortDateString());
+Console.WriteLine(time.ToShortTimeString());
+Console.WriteLine(time.ToLongDateString());
+Console.WriteLine(time.ToLongTimeString());
+
+Console.WriteLine(time.AddDays(5).ToLongDateString());
+Console.WriteLine(time.AddHours(5).ToLongDateString());
+Console.WriteLine(time.AddDays(-5).ToLongDateString());
+
+DateTime birthDay = DateTime.Parse("01/06/1997");
+TimeSpan age = DateTime.Now.Subtract(birthDay);
+Console.WriteLine(age.TotalDays);
