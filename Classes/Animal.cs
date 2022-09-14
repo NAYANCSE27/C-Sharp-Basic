08e4e0fb5ac3afd5abbe6ce459ffbb4171f3c20e
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-09-14 23:33:25 +0600

    basic clases in c#

---
+++ Classes/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Animal
+    {
+        public string name { get; set; }
+        public string color { get; set; }
+        public string sound { get; set; }
+
+        public decimal determineValue()
+        {
+            decimal value = 0;
+            if (color == "black")
+                value = 5000;
+            else
+                value = 2000;
+            return value;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,10 @@
+﻿using Classes;
+
+Animal myAnimal = new Animal();
+
+myAnimal.name = "bilay";
+myAnimal.color = "black";
+myAnimal.sound = "meow meow";
+
+Console.WriteLine("{0} {1} {2}", myAnimal.name, myAnimal.color, myAnimal.sound);
+Console.WriteLine("{0:C}", myAnimal.determineValue());
