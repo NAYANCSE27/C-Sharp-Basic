214d9cc4444fee307bbfd13dc0a39b494ab78e59
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-09-17 00:15:19 +0600

    working with object life time

---
+++ ObjectLifeTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using ObjectLifeTime;
+
+Car myCar = new Car();
+
+myCar.Make = "Oldmobile";
+myCar.Model = "Ross Royals";
+myCar.Year = 2022;
+myCar.Color = "Black";
+
+Car myOtherCar;
+myOtherCar = myCar;
+
+Console.WriteLine("{0} {1} {2} {3}", myOtherCar.Make, myOtherCar.Model, myOtherCar.Year, myOtherCar.Color);
+
+myOtherCar.Model = "BMW";
+
+Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
