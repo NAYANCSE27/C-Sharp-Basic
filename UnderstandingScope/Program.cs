d066b5945ba424ab8cf8a185f144ea768bc14ab7
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-09-18 23:01:03 +0600

    scope of different types of variable in c# program

---
+++ UnderstandingScope/Program.cs
@@ -0,0 +1,22 @@
+﻿using UnderstandingScope;
+
+otherClass oc = new otherClass();
+
+string k = "";
+string j = "";
+
+for(int i=0; i<10; i++)
+{
+    j = i.ToString();
+    k = i.ToString();
+    if(i==9)
+    {
+        string l=i.ToString();
+    }
+    //Console.WriteLine(l);
+}
+
+Console.WriteLine(j);
+Console.WriteLine(k);
+
+otherClass.helperMethod();
---
+++ UnderstandingScope/otherClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingScope
+{
+    internal class otherClass
+    {
+        public static void helperMethod()
+        {
+            Console.WriteLine("This is a helper method");
+        }
+    }
+}
