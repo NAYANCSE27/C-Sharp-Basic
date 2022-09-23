00135d8ae7eb3d2baeef751955301e0a95fad340
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-09-23 12:02:56 +0600

    file input/output and structure of c# project

---
+++ AssambliesAndNamespace/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+Console.WriteLine("Hello World");
+
+WebClient client = new WebClient();
+string text = client.DownloadString("http://msdn.microsoft.com");
+
+Console.WriteLine(text);
+
+File.WriteAllText(@"E:\C-Sharp-Basic\AssambliesAndNamespace\writeFile", text);
