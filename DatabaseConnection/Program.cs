0c31333ab7856e0199db54c0b99bb2e54cbaaa41
Others <Others@STUDENT>
2022-10-29 11:37:20 +0600

    some basic changes in all file for version update

---
+++ DatabaseConnection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server="localhost";
+            string database="testdb";
+            string username="root";
+            string password="root";
+
+            string constring="SERVER="+server+";"+
+                "DATABASE="+ database+";"+
+                "USERID="+ username+";"+
+
+        }
+    }
+}
