e30083ad9a3c80d63922607939fddecddf91b5d7
Others <Others@STUDENT>
2022-11-26 17:06:11 +0600

    file reading

---
+++ FileProcessing/Program.cs
@@ -0,0 +1,18 @@
+﻿
+/**
+// Read entire text file content into one string
+string text = File.ReadAllText("E:\\isnjb27\\c# project\\C-Sharp-Basic\\FileProcessing\\input.txt");
+Console.WriteLine(text);
+*/
+
+
+// Read a text file as line by line
+string[] lines = File.ReadAllLines("E:\\isnjb27\\c# project\\C-Sharp-Basic\\FileProcessing\\input.txt");
+
+foreach (string line in lines)
+    Console.WriteLine(line);
+
+
+
+
+Console.ReadLine();
