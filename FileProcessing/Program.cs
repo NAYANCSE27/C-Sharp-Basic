7435f251c57fb86a7fcc738efa982fc2582f25af
Others <Others@STUDENT>
2022-11-26 17:38:58 +0600

    file input and file output done, there are many other way to do this process

--- FileProcessing/Program.cs
+++ FileProcessing/Program.cs
@@ -5,14 +5,17 @@ string text = File.ReadAllText("E:\\isnjb27\\c# project\\C-Sharp-Basic\\FileProc
 Console.WriteLine(text);
 */
 
-
+/**
 // Read a text file as line by line
 string[] lines = File.ReadAllLines("E:\\isnjb27\\c# project\\C-Sharp-Basic\\FileProcessing\\input.txt");
 
 foreach (string line in lines)
     Console.WriteLine(line);
+*/
 
-
+ 
+string texts = File.ReadAllText("E:\\isnjb27\\c# project\\C-Sharp-Basic\\FileProcessing\\input.txt");
+File.WriteAllTextAsync("output.txt", texts);
 
 
 Console.ReadLine();
